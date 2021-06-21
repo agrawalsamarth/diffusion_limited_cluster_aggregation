1618288904a7d9b18706603602c981180e9aa9a9
agrawal <samarth.agrawal@empa.ch>
2021-06-21 10:52:56 +0200

    fixed params file error message

--- simulation_files/target/simulation.cpp
+++ simulation_files/target/simulation.cpp
@@ -4,10 +4,15 @@ int main(int argc, char *argv[])
 {
     //int file_number = atoi(argv[1]);
     //std::cout<<"start"<<std::endl;
+    
+    if (argc < 2){
+        std::cout<<"please provide params file"<<std::endl;
+        return 0;
+    }
 
     dlca_lattice_3d test(argv[1]);
     test.runSimulation();
-
+    
     if (argc == 3)
         test.save_config(argv[2]);
     else
