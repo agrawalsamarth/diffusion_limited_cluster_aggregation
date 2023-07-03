73e56834435ddb2600b795156656190f33bbcaf4
Samarth Agrawal <samarth.agrawal@empa.ch>
2023-07-03 13:01:30 +0200

    modified script

--- scripts/setup_simulation_rsp_2d.py
+++ scripts/setup_simulation_rsp_2d.py
@@ -90,11 +90,15 @@ def run_simulation(phi, L, seed_pct, rng_seed):
     config_filename=config_files_dir+str(rng_seed)+'.csv'
     command = 'simulation '+params_file+' '+config_filename        
     os.system(command)
+    #print(command)
     
     print("phi = {}, L={},rng_seed={} lbp".format(phi,L,rng_seed))
     
     rog_filename=results_dir+'lbp_'+str(rng_seed)+'.csv'
     command = 'lb_bonds_clusterwise_invA ' + config_filename +' ' + rog_filename
+    os.system(command)
+    
+    command = 'rm ' + config_filename
     os.system(command)
     
 def create_dirs_for_mk_table(dim):
@@ -140,54 +144,6 @@ def create_dirs_for_mk_table(dim):
         pass
     else:
         pass     
-
-def run_simulation_for_random_params(phi, L, rng_seed):
-    
-    lattice=1
-    x0_bc="periodic"
-    x1_bc="periodic"
-    x2_bc="periodic"
-    alpha=0.5
-    seedMass=1e12
-    seed_pct = 100
-
-    
-    params_file  = params_dir+'params_'+str(rng_seed)+'.csv'        
-    file = open(params_file, "w")
-    
-    file.write("system=random_site_percolation\n")
-    file.write("lattice="+str(lattice)+"\n")
-    file.write("aggregation_type=normal\n")
-    file.write("aggregation_condition=mass\n")
-    file.write("bind=normal\n")
-    file.write("movement=brownian\n")
-    file.write("agg_dist_tolerance=0.0\n")
-    file.write("D="+str(D)+"\n")
-    file.write("seed_pct="+str(seed_pct)+"\n")
-    file.write("x0_bc="+str(x0_bc)+"\n")
-    file.write("x1_bc="+str(x1_bc)+"\n")
-    file.write("x2_bc="+str(x2_bc)+"\n")
-    file.write("x0_L="+str(L)+"\n")
-    file.write("x1_L="+str(L)+"\n")
-    file.write("x2_L="+str(L)+"\n")
-    file.write("phi="+str(phi)+"\n")
-    file.write("alpha="+str(alpha)+"\n")
-    file.write("seedMass="+str(seedMass)+"\n")
-    file.write("rng_seed="+str(rng_seed)+"\n")        
-    file.close()
-    
-    print("phi = {}, N={},rng_seed={} simulation".format(phi,L,rng_seed))
-
-    config_filename=config_files_dir+str(rng_seed)+'.csv'
-    command = 'simulation '+params_file+' '+config_filename        
-    os.system(command)
-    
-    print("phi = {}, N={},rng_seed={} lbp".format(phi,L,rng_seed))
-    
-    rog_filename=results_dir+'lbp_'+str(rng_seed)+'.csv'
-    time_filename=results_dir+'time_'+str(rng_seed)+'.csv'
-    command = 'lb_bonds_clusterwise_invA ' + config_filename +' ' + rog_filename + ' ' + time_filename
-    os.system(command)    
 
 #phi=0.15    
 seeds=100
